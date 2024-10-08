d87cecd40e5c1de3c5fc7b3c3a9926eb4843b528
Gen Vikat <gen@veebisepad.ee>
2024-10-08 10:04:28 +0300

    WIP added sorting button

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <script setup>
 import axios from "axios";
 import { onMounted, ref } from "vue";
+import { AdjustmentsHorizontalIcon } from "@heroicons/vue/16/solid";
 
 const products = ref([]);
 const categories = ref([]);
 const activeCategory = ref("");
+const activeSort = ref(ASC)
+
+const ASC = "asc"
+const DESC = "desc"
+const DEFAULT = ""
 
 const fetchAllProducts = async () => {
     activeCategory.value = "Kõik"
-    const response = await axios.get("https://fakestoreapi.com/products");
+    const response = await axios.get(`https://fakestoreapi.com/products?sort=${activeSort.value}`);
     products.value = response.data;
 };
 
 const fetchProductsByCategory = async (category) => {
-    const response = await axios.get(`https://fakestoreapi.com/products/category/${category}`)
+    const response = await axios.get(`https://fakestoreapi.com/products/category/${category}?sort=${activeSort.value}`)
     products.value = response.data
 }
 
@@ -29,6 +35,13 @@ const handleCategoryChange = async (category) => {
     await fetchProductsByCategory(category);
 };
 
+const handleSort = () => {
+    // activeSort.value === DEFAULT ? ASC : activeSort === ASC ? DESC : DEFAULT
+    if (activeSort === DEFAULT) activeSort.value = ASC
+    if (activeSort === ASC) activeSort.value = DESC
+    activeSort.value = DEFAULT
+}
+
 const handleCartAdd = async (product) => {
     console.log(product);
 };
@@ -42,22 +55,30 @@ onMounted(async () => {
 <template>
     <section class="max-w-5xl mx-auto">
         <h1 class="text-2xl py-12 font-bold">Welcome to our fake store</h1>
-        <div class="flex items-center gap-6 mb-6">
+        <div class="flex items-center justify-between mb-6">
+            <div class="flex items-center gap-6">
+                <button
+                    @click="fetchAllProducts()"
+                    class="uppercase font-bold"
+                    :class="activeCategory === 'Kõik' ? 'text-black' : 'text-black/50'"
+                >
+                    Kõik
+                </button>
+                <button
+                    v-for="category in categories"
+                    :key="category"
+                    class="uppercase font-bold"
+                    :class="activeCategory === category ? 'text-black' : 'text-black/50'"
+                    @click="handleCategoryChange(category)"
+                >
+                    {{ category }}
+                </button>
+            </div>
+            {{ activeSort }}
             <button
-                @click="fetchAllProducts()"
-                class="uppercase font-bold"
-                :class="activeCategory === 'Kõik' ? 'text-black' : 'text-black/50'"
+                @click="handleSort"
             >
-                Kõik
-            </button>
-            <button
-                v-for="category in categories"
-                :key="category"
-                class="uppercase font-bold"
-                :class="activeCategory === category ? 'text-black' : 'text-black/50'"
-                @click="handleCategoryChange(category)"
-            >
-                {{ category }}
+                <AdjustmentsHorizontalIcon class="w-5 h-5 text-green-800" />
             </button>
         </div>
         <div class="grid grid-cols-4 gap-4">
